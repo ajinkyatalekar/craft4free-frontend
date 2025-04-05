651f56d366586bd4070d8bb1def251125a72a838
ajinkyatalekar <ajinkya.tal@gmail.com>
2025-04-05 00:57:21 -0400

    Update api paths

---
+++ src/services/api_old.ts
@@ -0,0 +1,20 @@
+const API_URL = import.meta.env.PROD
+  ? "http://api.craft4free.online/"
+  : "http://api.craft4free.online/";
+
+export async function createServer(server: string, access_token: string) {
+  try {
+    const response = await fetch(`${API_URL}servers?server=${server}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+
+    return response.json();
+  } catch (error) {
+    console.error("API request failed:", error);
+    throw error;
+  }
+}
